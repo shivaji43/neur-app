51a48cd330d754a313f42af32d3037e3f0cac647
pradyutf <145162667+pradyutf@users.noreply.github.com>
2025-01-14 03:06:30 +0530

    feat: Action Management (#116)
    * action-managemnt feature add
    
    * remove unnecessary logs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -40,3 +40,15 @@ export function throttle<T extends (...args: any[]) => any>(
     }
   };
 }
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  }).format(date);
+}
